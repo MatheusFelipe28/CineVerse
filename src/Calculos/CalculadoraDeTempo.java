101512c7782141ddd3831eca48000a3117918bb0
Matheus <matheusfelipegomesfernandes@gmail.com>
2023-08-16 00:14:51 -0300

    Subindo Aplicação

---
+++ src/Calculos/CalculadoraDeTempo.java
@@ -0,0 +1,27 @@
+package Calculos;
+
+import Model.Titulo;
+import Model.Filme;
+import Model.Serie;
+import Model.Episodio;
+
+public class CalculadoraDeTempo {
+    private int tempoTotal;
+
+    public int getTempoTotal() {
+        return this.tempoTotal;
+    }
+
+//    public void inclui(Filme f) {
+//        this.tempoTotal += f.getDuracaoEmMinutos();
+//    }
+//
+//    public void inclui(Serie s) {
+//        this.tempoTotal += s.getDuracaoEmMinutos();
+//    }
+
+    public void inclui(Titulo titulo) {
+        System.out.println("Adicionando duração em minutos de " + titulo);
+        this.tempoTotal += titulo.getDuracaoEmMinutos();
+    }
+}
